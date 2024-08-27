63484d6f866453bb30075680273952c9f149bcc6
haseeb hummo <haseebpersonalwork@gmail.com>
2024-08-27 12:00:00 +0530

    Update profileController: Implement validation with ExpressError and wrapAsync for error handling

--- backend/src/controllers/profileController.ts
+++ backend/src/controllers/profileController.ts
@@ -1,23 +1,43 @@
+// Node modules
+import { NextFunction, Request, Response } from "express";
+
+// Internal Models
 import Profile from "models/profileModel";
 
-const updateProfile =
-    async (req, res, next) => {
-        const { about, contactNumber, dateOfBirth, gender } = req.body;
-      
-        const profileId = req.user?.profile;
-    
-        const updatedProfile = await Profile.findByIdAndUpdate(profileId, {
-            about,
-            contactNumber,
-            dateOfBirth,
-            gender
-        }, { new: true, runValidators: true })
-    
-        return res.status(200).json({
-            success: true,
-            message: "User profile updated successfully",
-            profile: updatedProfile
-        });
-};
+// Schemas
+import { profileSchema, ProfileSchemaType } from "schemas/profileSchema";
+
+// Utilities
+import ExpressError from "utils/ExpressError";
+import wrapAsync from "utils/wrapAsync";
+
+
+const updateProfile = wrapAsync(
+  async (req: Request<{}, {}, ProfileSchemaType>, res: Response, next: NextFunction) => {
+    const { about, contactNumber, dateOfBirth, gender } = profileSchema.parse(req.body);
+  
+    const profileId = req.user?.profile;
+
+    const updatedProfile = await Profile.findByIdAndUpdate(profileId, {
+        about,
+        contactNumber,
+        dateOfBirth,
+        gender
+    }, { new: true, runValidators: true })
+
+    if(!updatedProfile) {
+        return next(new ExpressError(404, "User's Profile not found"));
+    };
+
+    return res.status(200).json({
+        success: true,
+        message: "User profile updated successfully",
+        profile: updatedProfile
+    });
+});
+
+// Profile picture update
+
+// Get iser details
 
 export { updateProfile };
